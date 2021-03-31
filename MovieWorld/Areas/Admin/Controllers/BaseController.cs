bba2a1e18c3c1c009a649ab03819c7ed5fcd0f05
ardasar94 <sarihanarda@gmail.com>
2021-03-31 13:46:15 +0300

    Add project files.

---
+++ MovieWorld/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using MovieWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieWorld.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public abstract class BaseController : Controller
+    {
+        protected ApplicationDbContext db = new ApplicationDbContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MovieWorld/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace MovieWorld.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MovieWorld.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MovieWorld.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
